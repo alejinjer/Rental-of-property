ece26f2f0deb20024cc09c2ff9a820b4fd805815
Nikolay Zhurba <gloomikon@gmail.com>
2020-05-18 19:41:41 +0300

    RentalApp init

---
+++ Client/RentalApp/RentalApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RentalApp
+//
+//  Created by Nikolay Zhurba on 18.05.2020.
+//  Copyright © 2020 alejinjer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
